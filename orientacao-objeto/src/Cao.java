5408b2cde001736462d74d93ed7fe0f2b2104230
Franccesco Antonio <franccesco_@hotmail.com>
2023-11-23 13:39:13 -0300

    métodos construtores e sobrecarregados

---
+++ orientacao-objeto/src/Cao.java
@@ -0,0 +1,23 @@
+public class Cao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+
+    public Cao() {
+        cor = "Caramelo";
+    }
+
+    public Cao(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void Anda() {
+        System.out.println("Estou andando " + cor);
+    }
+
+    public void DadosCao() {
+        System.out.println(nome + " " + idade);
+    }
+}
---
+++ orientacao-objeto/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cao cachorro = new Cao();
+        cachorro.Anda();
+    
+        Cao dog = new Cao("Lulu", 12);
+        dog.DadosCao();
+    }
+}
