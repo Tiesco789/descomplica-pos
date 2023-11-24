4b15e2359f56dddba35acb92e83bb1e700ef0e82
Franccesco Antonio <franccesco_@hotmail.com>
2023-11-24 11:41:10 -0300

    conceitos de herança

--- heranca/src/App.java
+++ heranca/src/App.java
@@ -1,11 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        Pessoa p = new Pessoa();
+        // Pessoa p = new Pessoa();
 
-        p.nome = "Yan";
+        // p.nome = "Yan";
 
-        Vendedor v = new Vendedor();
-        v.nome = "Laís";
-        v.comissao = 8.6;
+        // Vendedor v = new Vendedor();
+        // v.nome = "Laís";
+        // v.comissao = 8.6;
+
+        Pedido p = new Pedido();
     }
 }
