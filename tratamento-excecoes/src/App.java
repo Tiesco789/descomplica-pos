c6387a8f0cb66d7d913d44d83d5a780928713c0d
Franccesco Antonio <franccesco_@hotmail.com>
2023-11-24 15:03:16 -0300

    Tratamento de exceções

--- tratamento-excecoes/src/App.java
+++ tratamento-excecoes/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite um valor inteiro: ");
+
+            int num1 = entrada.nextInt();
+            System.out.println(num1);
+        } catch (Exception ex) {
+            System.out.println("Erro, valor digitado não é um número inteiro");
+        }
     }
 }
