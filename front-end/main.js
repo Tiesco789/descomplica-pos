f4bf6c42b06cf2cc9fe9103f217a37dc65165cb3
Franccesco Antonio <franccesco_@hotmail.com>
2023-11-30 13:48:21 -0300

    Arrays: Filter

--- front-end/main.js
+++ front-end/main.js
@@ -66,9 +66,88 @@
 // pais.pop();
 // pais.push();
 
-var mesesDoAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+// var mesesDoAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
 
-for (let i = 0; i < 4; i++) {
-    const trimestre = mesesDoAno.splice(0, 3);
-    console.log(trimestre);
-}
+// for (let i = 0; i < 4; i++) {
+//     const trimestre = mesesDoAno.splice(0, 3);
+//     console.log(trimestre);
+// }
+
+// var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// var resultado = numeros.filter(item => item % 2 === 0);
+// console.log(resultado);
+
+// var numerosFiltrados = numeros.filter(
+//     function (valor) {
+//         return valor > 5;
+//     }
+// );
+// console.log(numerosFiltrados);
+
+// function buscarValores(valor) {
+//     return valor < 5;
+// }
+
+// var numerosEcontrados = numeros.filter(buscarValores);
+// console.log(numerosEcontrados);
+
+// var r1 = numeros.filter((valor) => {
+//     return valor < 5;
+// });
+// console.log(r1);
+
+// var r2 = numeros.filter((valor) => valor < 5);
+
+// var funcionario = [
+//     {
+//         nome: "Luis",
+//         idade: 62
+//     },
+//     {
+//         nome: "Davi",
+//         idade: 22
+//     },
+//     {
+//         nome: "Arthur",
+//         idade: 18
+//     },
+//     {
+//         nome: "Lucas",
+//         idade: 40
+//     }
+// ];
+
+// var pessoasListagem = funcionario.filter(
+//     function (valor) {
+//         return valor.nome.length < 5;
+//     }
+// );
+// console.log(pessoasListagem);
+
+
+var produtos = [
+    {
+        id: 1,
+        descricao: "Smartphone",
+        categoria: "Eletronico"
+    },
+    {
+        id: 2,
+        descricao: "Notebook",
+        categoria: "Eletronico"
+    },
+    {
+        id: 3,
+        descricao: "Geladeira",
+        categoria: "Eletrodomestico"
+    },
+];
+
+var eletronicos = produtos.filter((el) => {
+    if (el.categoria === "Eletronico") {
+        return el.descricao
+    }
+});
+
+console.log(eletronicos);
