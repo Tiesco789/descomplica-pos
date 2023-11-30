7e41514add671863ffdc5b6ce3b7e76bbec44f12
Franccesco Antonio <franccesco_@hotmail.com>
2023-11-30 12:06:34 -0300

    Exercicio: trimestre

--- front-end/main.js
+++ front-end/main.js
@@ -30,38 +30,45 @@
 
 // console.log(maiorCidade);
 
-var arr1 = [1, 2, 3, 4, 5, 6];
-console.log(arr1.join(' '));
+// var arr1 = [1, 2, 3, 4, 5, 6];
+// console.log(arr1.join(' '));
 
-var retirado = arr1.shift(); // Retira o primeiro item do array
-console.log(retirado);
+// var retirado = arr1.shift(); // Retira o primeiro item do array
+// console.log(retirado);
 
-console.log(arr1);
+// console.log(arr1);
 
-console.log("Posição do numero: " + arr1.indexOf(3));
+// console.log("Posição do numero: " + arr1.indexOf(3));
 
-arr1.push(7); // Adiciona no ultimo indice do array
-console.log(arr1);
+// arr1.push(7); // Adiciona no ultimo indice do array
+// console.log(arr1);
 
-arr1.pop(); // Retira o ultimo item do array
-console.log(arr1);
+// arr1.pop(); // Retira o ultimo item do array
+// console.log(arr1);
 
 // Separar numeros positivos de negativos e retorne apenas os positivos
-var ar = [-5, 10, 15, 20, -3, 89];
+// var ar = [-5, 10, 15, 20, -3, 89];
 
 
-var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-arr2.splice(3, 8);
-console.log(arr2);
+// var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr2.splice(3, 8);
+// console.log(arr2);
 
-var nomes = ["maria", "joao", "lucas", "pedro"];
-console.log(nomes);
+// var nomes = ["maria", "joao", "lucas", "pedro"];
+// console.log(nomes);
 
-var novos = nomes.splice(1, 2, "luiz", "ronaldo");
-console.log(novos);
+// var novos = nomes.splice(1, 2, "luiz", "ronaldo");
+// console.log(novos);
 
-var pais = ["brasil", "argentina", "colombia"];
-pais.unshift("uruguai")
-console.log(pais);
-pais.pop();
-pais.push();
+// var pais = ["brasil", "argentina", "colombia"];
+// pais.unshift("uruguai")
+// console.log(pais);
+// pais.pop();
+// pais.push();
+
+var mesesDoAno = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+
+for (let i = 0; i < 4; i++) {
+    const trimestre = mesesDoAno.splice(0, 3);
+    console.log(trimestre);
+}
